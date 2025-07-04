8f567f02dfd558703c4635b42a634f642c006103
kkky <kkkykin@foxmail.com>
2025-07-04 17:19:07 +0800

    chore: tags

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((etags-regen-file-extensions . ("org")))))
